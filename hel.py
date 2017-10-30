948576166df33062911872ef124fea284aa6b2f3
rajmuchhala <31999183+rajmuchhala@users.noreply.github.com>
2017-10-30 11:26:34 -0400

    Update hel.py

--- hel.py
+++ hel.py
@@ -1,17 +1,2 @@
 import tensorflow as tf
 print("Hello tensorflow is installed and ready to go")
-# Initialize two constants
-x1 = tf.constant([1,2,3,4])
-x2 = tf.constant([5,6,7,8])
-
-# Multiply
-result = tf.multiply(x1, x2)
-
-# Intialize the Session
-sess = tf.Session()
-
-# Print the result
-print(sess.run(result))
-
-# Close the session
-sess.close()
